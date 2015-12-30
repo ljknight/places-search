61e404988f689da510bf92ad563580d8039e6410
Laura Knight <ljknight@gmail.com>
2015-12-30 03:49:57 -0800

    Display results upon search

--- client/finder/finder.js
+++ client/finder/finder.js
@@ -7,66 +7,45 @@ angular.module('places.finder', [])
     var map;
 
     function initMap() {
-      var pyrmont = {lat: -33.866, lng: 151.196};
+      // Map necessary for Places
+      map = new google.maps.Map(document.getElementById('map'), {});
+    }
 
-      map = new google.maps.Map(document.getElementById('map'), {
-        center: pyrmont,
-        zoom: 17
-      });
-
+    $scope.submit = function() {
+      var request = {
+        query: $scope.place
+      };
       var service = new google.maps.places.PlacesService(map);
-      service.nearbySearch({
-        location: pyrmont,
-        radius: 500,
-        types: ['store']
-      }, processResults);
+      service.textSearch(request, callback);
+    };
+
+    function callback(results, status) {
+      $scope.results = results;
+      // Force digest after AJAX response
+      $scope.$apply();
     }
 
-    function processResults(results, status, pagination) {
-      if (status !== google.maps.places.PlacesServiceStatus.OK) {
-        return;
-      } else {
-        createMarkers(results);
+    // function processResults(results, status, pagination) {
+    //   if (status !== google.maps.places.PlacesServiceStatus.OK) {
+    //     return;
+    //   } else {
+    //     $scope.results = results;
+    //     console.log($scope.results)
+    //     createMarkers(results);
 
-        // if (pagination.hasNextPage) {
-        //   var moreButton = document.getElementById('more');
+    //     if (pagination.hasNextPage) {
+    //       var moreButton = document.getElementById('more');
 
-        //   moreButton.disabled = false;
+    //       moreButton.disabled = false;
 
-        //   moreButton.addEventListener('click', function() {
-        //     moreButton.disabled = true;
-        //     pagination.nextPage();
-        //   });
-        // }
-      }
-    }
+    //       moreButton.addEventListener('click', function() {
+    //         moreButton.disabled = true;
+    //         pagination.nextPage();
+    //       });
+    //     }
+    //   }
+    // }
 
-    function createMarkers(places) {
-      var bounds = new google.maps.LatLngBounds();
-      var placesList = document.getElementById('places');
-
-      for (var i = 0, place; place = places[i]; i++) {
-        var image = {
-          url: place.icon,
-          size: new google.maps.Size(71, 71),
-          origin: new google.maps.Point(0, 0),
-          anchor: new google.maps.Point(17, 34),
-          scaledSize: new google.maps.Size(25, 25)
-        };
-
-        var marker = new google.maps.Marker({
-          map: map,
-          icon: image,
-          title: place.name,
-          position: place.geometry.location
-        });
-
-        placesList.innerHTML += '<li>' + place.name + '</li>';
-
-        bounds.extend(place.geometry.location);
-      }
-      map.fitBounds(bounds);
-    }
   initMap();
   }
 ]);
