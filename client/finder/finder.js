0c26ff7b82f73d0acbe62a4432f65fe817b8fbcc
Laura Knight <ljknight@gmail.com>
2015-12-30 18:11:37 -0800

    Show map on load, display markers from search on map

--- client/finder/finder.js
+++ client/finder/finder.js
@@ -1,51 +1,94 @@
 angular.module('places.finder', [])
 
-.controller('FinderController', ['$scope', '$window',
-  function($scope, $window) {
+.controller('FinderController', ['$scope', '$window', '$state',
+  function($scope, $window, $state) {
 
-    // Google Map & Places reference: https://developers.google.com/maps/documentation/javascript/examples/places-placeid-finder
+    var place;
+    var placeID;
+    var placeName;
+    var infoWindow = new google.maps.InfoWindow({maxWidth: 175});
+    var service;
+    var bounds;
     var map;
+    $scope.results = [];
 
-    function initMap() {
-      // Map necessary for Places
-      map = new google.maps.Map(document.getElementById('map'), {});
-    }
+    var initMap = function() {
+      // Default to SF
+      var mapOptions = {
+          center: {
+            lat: 37.783542,
+            lng: -122.408943
+          },
+          zoom: 13
+        };
+
+      map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    };
 
     $scope.submit = function() {
+      $scope.results = [];
+      bounds = new google.maps.LatLngBounds();
+      
       var request = {
         query: $scope.place
       };
-      var service = new google.maps.places.PlacesService(map);
-      service.textSearch(request, callback);
+      
+      service = new google.maps.places.PlacesService(map);
+      service.textSearch(request, processResults);
+      initMap();
     };
 
-    function callback(results, status) {
-      $scope.results = results;
-      // Force digest after AJAX response
-      $scope.$apply();
-    }
+    var processResults = function(results, status, pagination) {
+      if (status !== google.maps.places.PlacesServiceStatus.OK) {
+        return;
+      } else {
+        for (var i = 0; i < results.length; i++) {
+          $scope.results.push(results[i]);
+          // Force digest to render list
+          $scope.$apply();
+          createMarkers(results[i]);
+        }
 
-    // function processResults(results, status, pagination) {
-    //   if (status !== google.maps.places.PlacesServiceStatus.OK) {
-    //     return;
-    //   } else {
-    //     $scope.results = results;
-    //     console.log($scope.results)
-    //     createMarkers(results);
+        // if (pagination.hasNextPage) {
+        //   $scope.more = true;
+        //   // Force digest to render button
+        //   $scope.$apply();
 
-    //     if (pagination.hasNextPage) {
-    //       var moreButton = document.getElementById('more');
+        //   var moreButton = document.getElementById('more');
 
-    //       moreButton.disabled = false;
+        //   moreButton.addEventListener('click', function() {
+        //     pagination.nextPage();
+        //   });
+        // } else {
+        //   $scope.more = false;
+        // }
+      }
+    };
 
-    //       moreButton.addEventListener('click', function() {
-    //         moreButton.disabled = true;
-    //         pagination.nextPage();
-    //       });
-    //     }
-    //   }
-    // }
+    var createMarkers = function(place) {
+      var marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location
+      });
 
-  initMap();
+      google.maps.event.addListener(marker, 'click', function() {
+        service.getDetails(place, function(result, status) {
+          if (status !== google.maps.places.PlacesServiceStatus.OK) {
+            console.error(status);
+            return;
+          }
+          infoWindow.setContent(result.name);
+          infoWindow.open(map, marker);
+        });
+      });
+      bounds.extend(place.geometry.location);
+      map.fitBounds(bounds);
+    };
+  
+  // Loads map on state change
+    $scope.$on('$stateChangeSuccess', function() {
+      initMap();
+    });
+
   }
 ]);
